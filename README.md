c1dc40f306798be49bd400576855a441eb8feca3
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-26 18:47:50 +0530

    ..

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # audiora
 Audiora makes it easy to enjoy podcasts and read articles, with smooth playback and fast-loading content all in one place
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>audiora</div>;
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4 ">Welcome to Audiora</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Link
+          href="/podcasts"
+          className="p-4 border rounded-lg hover:bg-gray-100"
+        >
+          <h2 className="text-xl font-semibold">Podcasts</h2>
+          <p>Search and listen to your favorite podcasts while reading</p>
+        </Link>
+        
+        <Link href="/blog" className="p-4 border rounded-lg hover:bg-gray-100">
+          <h2 className="text-xl font-semibold">Blog</h2>
+          <p>Read our latest articles</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
-
-
-
